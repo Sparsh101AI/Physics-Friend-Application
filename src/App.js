dc03937467f679f2fdac96ac4f5c863d3f3a3308
Sparsh101AI <sparshpatel6165@gmail.com>
2021-09-05 15:55:16 -0600

    added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 import {
   BrowserRouter as Router,
-  Switch,
+  
   Route,
-  Link
+ 
 } from "react-router-dom";
 import HomePage from './HomePage/HomePage';
 import AboutUs from './AboutUs/AboutUs';

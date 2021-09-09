1588b87a07c288aa5c916483c750bdebdeb9342d
Darsh <darshpatelcr7@gmail.com>
2021-09-09 08:24:05 -0600

    k

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,42 +1,24 @@
-import './Nav.css';
-import Container from 'react-bootstrap/Container';
-import { Link } from 'react-router-dom';
-import Navbar from 'react-bootstrap/Navbar';
+import "./Nav.css";
+import Container from "react-bootstrap/Container";
+import { Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
 
-
-
-
-
-function Nav(){
-
-    return(
-        
-
-        
+function Nav() {
+  return (
     <div>
-    
-        <Navbar className="Navbarcolor">
-    <Container>
-      
+      <Navbar className="Navbarcolor">
+        <Container>
+          <Link className="btn1" to="/">
+            <p> Home Page </p>
+          </Link>
 
-
-<Link className="btn1" to='/'>
-
-<p > Home Page </p>
-</Link>
-
-<Link className="btn2" to='/About'>
-<p > About Us </p>
-</Link>
-
-    </Container>
-  </Navbar>
-
+          <Link className="btn2" to="/AboutUs">
+            <p> About Us </p>
+          </Link>
+        </Container>
+      </Navbar>
     </div>
-        
-    )
-
-
+  );
 }
 
 export default Nav;

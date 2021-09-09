a04b5f0553c6bd5ba0cdc74e9aaa5b79b5923b22
Sparsh101AI <sparshpatel6165@gmail.com>
2021-09-09 08:26:32 -0600

    add

--- src/AboutUs/AboutUs.js
+++ src/AboutUs/AboutUs.js
@@ -8,7 +8,7 @@ import Image from "react-bootstrap/Image";
 function AboutUs() {
   return (
     <Container className="background">
-      <Image src="Background3.0.jpg" fluid />
+      <Image src="logoM.png" fluid />
       <Card className="About">
         <h1 className="Header">About Us</h1>
         <p className="Body">

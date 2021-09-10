bc33738b5a57c2b0993785f0a4383d92508b1c52
Darsh <darshpatelcr7@gmail.com>
2021-09-09 22:01:57 -0600

    Finished About Page

--- src/AboutUs/AboutUs.js
+++ src/AboutUs/AboutUs.js
@@ -7,30 +7,25 @@ import Image from "react-bootstrap/Image";
 
 function AboutUs() {
   return (
-    <Container className="background">
-      <Image src="logoM.png" fluid />
-      <Card className="About">
-        <h1 className="Header">About Us</h1>
-        <p className="Body">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ac
-          malesuada risus. Integer sapien velit, euismod id ornare ut, efficitur
-          ac felis. Mauris congue tristique eros, sit amet iaculis lectus
-          accumsan in. Donec auctor consequat luctus. In placerat ante
-          scelerisque sapien sollicitudin, non tincidunt ipsum faucibus.
-          Pellentesque ut est ut tortor efficitur lobortis in eu ligula.
-          Curabitur eros mi, iaculis nec urna eget, volutpat viverra metus.
-          Etiam scelerisque orci augue, suscipit interdum dui convallis sed.
-          Aenean tincidunt ex nulla, non hendrerit lorem convallis quis. Nunc
-          scelerisque scelerisque enim quis rhoncus. Vestibulum in porta nisi.
-          Curabitur sagittis, mauris nec venenatis mollis, odio nulla imperdiet
-          orci, quis euismod elit nibh a sem. Vivamus elit nisl, accumsan mollis
-          dignissim vitae, finibus eu augue. Praesent id pellentesque magna, non
-          suscipit sapien. Vivamus dignissim pretium velit a volutpat. Etiam sed
-          turpis id ex cursus commodo vel nec tortor. Sed nec felis vel justo
-          gravida lacinia sit amet at nisl.
-        </p>
-      </Card>
-    </Container>
+    <Card className="box">
+      <Card.Title className="title">About Us</Card.Title>
+      <Card.Title className="title2">Our Mission:</Card.Title>
+      <Card.Text className="text">
+        Physics Friend was created to help aid students in solving physics
+        problems. Although the Physics Calculator tool was specifically designed
+        for the Alberta Physics 20 Curriculum, we hope that Physics Friend can
+        be of use to many students around the world.
+      </Card.Text>
+      <Card.Title className="title2">Who We Are:</Card.Title>
+      <Card.Text className="text">
+        We are two grade twelve students with an interest in physics and a
+        passion for computer science. We thought of creating Physics Friend in
+        grade eleven while working on physics assignments (What a coincidence!)
+        and noticing that their were no tools readily avaliable to assist us in
+        solving physics problems. So instead of waiting for a physics solver to
+        be created we decided to make our own!
+      </Card.Text>
+    </Card>
   );
 }
 

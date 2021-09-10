729ddabb552e95eff5eba45a9a5625fccb368975
Darsh <darshpatelcr7@gmail.com>
2021-09-09 22:25:28 -0600

    Made some changes to the Navbar

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,52 +1,47 @@
 import "./Nav.css";
 import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
-import Navbar from 'react-bootstrap/Navbar'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from 'react-bootstrap/Nav'
-
+import Navbar from "react-bootstrap/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Nav from "react-bootstrap/Nav";
 
 function Nav1() {
   return (
     <div>
-        <Navbar collapseOnSelect expand="lg" className="nav-color1" variant="dark">
-    
-    <Container>
-    <Navbar.Brand >
-        <img
-          alt=""
-          src="logoM.png"
-          width="170"
-          height="70"
-          className="d-inline-block align-top"
-        />{' '}
-        </Navbar.Brand>
-    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    <Navbar.Collapse id="responsive-navbar-nav">
-      <Nav className="auto">
-       
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        className="nav-color1"
+        variant="dark"
+      >
+        <Container>
+          <Navbar.Brand>
+            <img
+              alt=""
+              src="logoM.png"
+              width="170"
+              height="70"
+              className="d-inline-block align-top"
+            />{" "}
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="auto">
+              <Link to="/" className="auto1">
+                <li className="button-color"> Home</li>
+              </Link>
 
+              <Link to="/AboutUs" className="auto1">
+                <li className="button-color">Physics Calculator</li>
+              </Link>
 
-      <Link to='/' className="auto1"> 
-      <li className="button-color" > Home</li> 
-       </Link>
-
-       
-     <Link to='/AboutUs' className="auto1">
-      <li className="button-color"  >Calculator</li>   
-      </Link> 
-
-      <Link to='/AboutUs' className="auto1">
-      <li className="button-color"  >AboutUs</li>   
-      </Link> 
-      
-  
-      </Nav>
-     
-    </Navbar.Collapse>
-    </Container>
-  </Navbar>
-  
+              <Link to="/AboutUs" className="auto1">
+                <li className="button-color">About Us</li>
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 }

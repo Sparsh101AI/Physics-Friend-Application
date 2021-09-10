5fb1b402c61edd4593bdff6d0fa4a2b686ea8919
Sparsh101AI <sparshpatel6165@gmail.com>
2021-09-10 09:02:54 -0600

    added

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -1,7 +1,6 @@
 import './Calculator.css';
-
-
-
+import CardGroup from 'react-bootstrap/CardGroup'
+import Card from 'react-bootstrap/Card'
 
 
 
@@ -15,9 +14,62 @@ function Calculator(){
 
         
     <div>
-        
-This Is THe Calculator
+         <br/>
+       
+        <div >
+        <p className="header1">Select A Topic!</p>
+            
+        </div>
 
+      
+       <br/>
+       
+
+
+
+        <CardGroup >
+  <Card className="cardg">
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </Card.Text>
+    </Card.Body>
+   
+  </Card>
+  <Card className="cardg"> 
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+      </Card.Text>
+    </Card.Body>
+   
+  </Card>
+  <Card className="cardg">
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+   
+  </Card>
+</CardGroup>
+
+
+<br/>
+
+<br/>
+
+<br/>
 
 
     </div>

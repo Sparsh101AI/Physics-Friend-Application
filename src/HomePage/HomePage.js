f1140f8002ee162cef898584ec7a81d9d555180b
Darsh <darshpatelcr7@gmail.com>
2021-09-10 18:25:10 -0600

    Added bs to homepage

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -5,7 +5,14 @@ function HomePage() {
   return (
     <Card className="homeBody">
       <Card.Title className="homeTitle">Welcome to Physics Friend</Card.Title>
-      <Card.Text className="homeText">Hello</Card.Text>
+      <Card.Title className="homeTitle2">
+        Solve All Types Of Types of Physics Equations!
+      </Card.Title>
+      <Card.Text className="homeText">
+        Try out our new Physics Problem Solver. Capable of solving basic to
+        advanced physics problems. Simply input the data that the problem
+        provides and our solver will take care of the rest!
+      </Card.Text>
     </Card>
   );
 }

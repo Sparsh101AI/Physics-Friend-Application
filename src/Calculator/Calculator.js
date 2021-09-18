059650a5c6b075055a4945102cf6392e17352db9
Darsh <darshpatelcr7@gmail.com>
2021-09-18 17:17:00 -0600

    Formatting

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -22,40 +22,49 @@ function Calculator() {
               <Card.Title className="calcTitle" id="kinematics">
                 Kinematics
               </Card.Title>
-
               <Card.Text className="calcText" id="kinematics">
                 Solve common word problems and equations which pretain to
                 kinematics. Problems which fall under kinematics include
                 variables such as velocity, distance, acceleration, and time.
+                <br />
+                <br />
               </Card.Text>
             </Card.Body>
           </Card>
         </Link>
-
-        <Card className="cardg">
-          <Card.Body id="dynamics">
-            <Card.Title className="calcTitle" id="dynamics">
-              Dynamics
-            </Card.Title>
-            <Card.Text className="calcText" id="dynamics">
-              Solve common word problems and equations which pretain to
-              dynamics. Problems which fall under dynamics include variables
-              such as force, mass, gravity, acceleration, and friction.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-
-        <Card className="cardg">
-          <Card.Body id="cM">
-            <Card.Title className="calcTitle" id="cM">
-              Circular Motion
-            </Card.Title>
-            <Card.Text className="calcText" id="cM">
-              <br />
-              Coming Soon...
-            </Card.Text>
-          </Card.Body>
-        </Card>
+        <Link className="linkcolor" to="/Kinematics">
+          <Card className="cardg">
+            <Card.Body id="dynamics">
+              <Card.Title className="calcTitle" id="dynamics">
+                Dynamics
+              </Card.Title>
+              <Card.Text className="calcText" id="dynamics">
+                Solve common word problems and equations which pretain to
+                dynamics. Problems which fall under dynamics include variables
+                such as force, mass, gravity, acceleration, and friction.
+                <br />
+                <br />
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Link>
+        <Link className="linkcolor" to="/Kinematics">
+          <Card className="cardg">
+            <Card.Body id="cM">
+              <Card.Title className="calcTitle" id="cM">
+                Circular Motion
+              </Card.Title>
+              <Card.Text className="calcText" id="cM">
+                <br />
+                Coming Soon...
+                <br />
+                <br />
+                <br />
+                <br />
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Link>
       </CardGroup>
 
       <br />

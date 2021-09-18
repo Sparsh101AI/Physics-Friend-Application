f8597b18940dc7a3a24d4c61bad4392333962289
Sparsh101AI <sparshpatel6165@gmail.com>
2021-09-18 17:05:29 -0600

    add

--- src/Kinematics/Kinematics.js
+++ src/Kinematics/Kinematics.js
@@ -17,7 +17,7 @@ function Kinematics() {
     <Card.Body>
       <Card.Title>Card title</Card.Title>
       <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
+        This is a widder card with supporting text below as a natural lead-in to
         additional content. This content is a little bit longer.
       </Card.Text>
     </Card.Body>

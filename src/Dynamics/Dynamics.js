8d77926ca8fed9c6cec399118d95d94c383cc70c
Darsh <darshpatelcr7@gmail.com>
2021-10-06 18:53:47 -0600

    added dynamics cards

--- src/Dynamics/Dynamics.js
+++ src/Dynamics/Dynamics.js
@@ -1,14 +1,61 @@
 import "./Dynamics.css";
-
-
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
+import React from "react";
 
 function Dynamics() {
   return (
     <div>
-   
-WElcome to dynamics
-    <br/>
-    <br/>
+      <div>
+        <CardGroup>
+          <Card className="kcard">
+            <Card.Body>
+              <Card.Title> Template</Card.Title>
+              <Card.Text>Lorem Impsum</Card.Text>
+            </Card.Body>
+            <Card.Footer>Foot</Card.Footer>
+          </Card>
+          <Card className="kcard">
+            <Card.Body>
+              <Card.Title> Template</Card.Title>
+              <Card.Text>Lorem Impsum</Card.Text>
+            </Card.Body>
+            <Card.Footer>Foot</Card.Footer>
+          </Card>
+          <Card className="kcard">
+            <Card.Body>
+              <Card.Title> Template</Card.Title>
+              <Card.Text>Lorem Impsum</Card.Text>
+            </Card.Body>
+            <Card.Footer>Foot</Card.Footer>
+          </Card>
+        </CardGroup>
+      </div>
+      <div>
+        <CardGroup>
+          <Card className="kcard">
+            <Card.Body>
+              <Card.Title> Template</Card.Title>
+              <Card.Text>Lorem Impsum</Card.Text>
+            </Card.Body>
+            <Card.Footer>Foot</Card.Footer>
+          </Card>
+          <Card className="kcard">
+            <Card.Body>
+              <Card.Title> Template</Card.Title>
+              <Card.Text>Lorem Impsum</Card.Text>
+            </Card.Body>
+            <Card.Footer>Foot</Card.Footer>
+          </Card>
+          <Card className="kcard">
+            <Card.Body>
+              <Card.Title> Template</Card.Title>
+              <Card.Text>Lorem Impsum</Card.Text>
+            </Card.Body>
+            <Card.Footer>Foot</Card.Footer>
+          </Card>
+        </CardGroup>
+      </div>
     </div>
   );
 }
